0ed0679b82867ea1ad48cc456a1bf6d4fc212956
prateek980ti <pdawalia@gmail.com>
2024-09-22 16:22:47 +0530

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<main className="flex flex-col gap-24 items-center p-10">
 			<h1 className="text-3xl font-bold text-center underline text-primary">
-				Bajaj Finserv Health Dev Challenge
+				Bajaj Finserv Assignment
 			</h1>
 			<HomeForm />
 		</main>
